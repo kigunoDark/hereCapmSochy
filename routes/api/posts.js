d76f468698116808f22254f96ea08073da5f44fa
foxBuka <krushenitskii@gmail.com>
2018-08-10 12:16:56 +0300

    get  and delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
-//Post moderl
+//Post model
 const Post = require("../../models/Post");
+
+//Post profile
+const Profile = require("../../models/Profile");
 
 //Validation
 const validatePostInput = require("../../validation/post");
@@ -13,6 +16,32 @@ const validatePostInput = require("../../validation/post");
 //@desc Test  post route
 //@access Publis
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+//@route GET api/posts
+//@desc Get post
+//@access Public
+
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(404).json({ nonpostsfound: "No posts found with that id" })
+    );
+});
+
+//@route GET api/posts/:id
+//@desc Get post by id
+//@access Public
+
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .sort({ date: -1 })
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nonpostfound: "No post found with that id" })
+    );
+});
 
 //@route POST api/posts
 //@desc Create post
@@ -39,4 +68,28 @@ router.post(
   }
 );
 
+//@route DELETE api/posts/:id
+//@desc Delete post
+//@access Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          //Check for post ovner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          //Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
 module.exports = router;
