10f334d29a661e1a431bec70f89b5d538771e053
foxBuka <krushenitskii@gmail.com>
2018-08-12 23:05:30 +0300

    front-end 0.1

--- client/src/components/layout/landing.js
+++ client/src/components/layout/landing.js
@@ -4,7 +4,33 @@ class Landing extends Component {
   render() {
     return (
       <div>
-        <h1> Здесь будет лендинг </h1>
+        <div id="index-banner" className="parallax-container">
+          <div className="section no-pad-bot">
+            <div className="container">
+              <h1 className="header center white-text text-lighten-2">
+                CheckOut
+              </h1>
+              <div className="row center">
+                <h5 className="header col s12 light">
+                  A modern interactive map, which chane you life in two clicks
+                </h5>
+              </div>
+              <div className="row center">
+                <a
+                  href="#"
+                  id="download-button"
+                  className="btn-large waves-effect waves-light blue lighten-1"
+                >
+                  Get Started
+                  <i className="material-icons left ">person_pin_circle </i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="parallax">
+            <img src="../img/header.jpg" />{" "}
+          </div>
+        </div>
       </div>
     );
   }
--- client/src/components/layout/navbar.js
+++ client/src/components/layout/navbar.js
@@ -3,21 +3,36 @@ import React, { Component } from "react";
 class Navbar extends Component {
   render() {
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <a href="#" className="brand-logo">
-            CheckOut
-          </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a href="badges.html">Login</a>
-            </li>
-            <li>
-              <a href="collapsible.html">Register</a>
-            </li>
-          </ul>
+      <div className="navbar">
+        <div className="nav-fixed">
+          <nav className="blue">
+            <div className="nav-wrapper container">
+              <a href="#" className="brand-logo">
+                CheckOut
+              </a>
+              <ul className="right hide-on-med-and-down">
+                <li>
+                  <a href="#"> Login </a>
+                </li>
+                <li>
+                  <a href="#"> Register </a>
+                </li>
+              </ul>
+              <ul id="nav-mobile" class="sidenav">
+                <li>
+                  <a href="#"> Login </a>
+                </li>
+                <li>
+                  <a href="#"> Register </a>
+                </li>
+              </ul>
+              <a href="#" data-target="nav-mobile" class="sidenav-trigger">
+                <i class="material-icons">menu</i>
+              </a>
+            </div>
+          </nav>
         </div>
-      </nav>
+      </div>
     );
   }
 }
